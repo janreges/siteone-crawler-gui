54949b98d358f6de18d1ad2dc3751dfa08b8954d
jozef.reges <jozef.reges@siteone.cz>
2023-11-29 18:42:57 +0100

    feat(CrawlerMiniStatsData): Added a new typescript class with predefined example values

---
+++ src/renderer/src/types/CrawlerMiniStatsData.ts
@@ -0,0 +1,22 @@
+export class CrawlerMiniStatsData {
+    public htmlTimeAvg: string = '320 ms';
+    public htmlTimeMin: string = '45 ms';
+    public htmlTimeMax: string = '1372 ms';
+    public code200x: number = 1;
+    public code300x: number = 5;
+    public code400x: number = 24;
+    public code500x: number = 2;
+    public codeErr: number = 0;
+    public contentHtmlNumber: number = 14;
+    public contentHtmlSize: string = '2.3 MB';
+    public contentImgNumber: number = 5;
+    public contentImgSize: string = '5.3 MB';
+    public contentJsNumber: number = 5;
+    public contentJsSize: string = '890 kB';
+    public contentCssNumber: number = 3;
+    public contentCssSize: string = '1.3 MB';
+    public contentFontsNumber: number = 1;
+    public contentFontsSize: string = '750 kB';
+    public contentFilesNumber: number = 10;
+    public contentFilesSize: string = '9.7 MB';
+}
