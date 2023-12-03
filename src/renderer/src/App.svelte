bd6196d9204952b1201d36261cfba547f564a7f2
jan.reges <jan.reges@siteone.cz>
2023-12-03 22:12:37 +0100

    app: added footer with mailto and link to crawler.siteone.io

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import CrawlerForm from "./components/CrawlerForm.svelte";
     import {onMount, onDestroy} from 'svelte';
+    import { VERSION } from './types/CrawlerInfo';
 
     let windowWidth: number = window.innerWidth;
     let windowHeight: number = window.innerHeight;
+    let osPlatform: string = window.api.getPlatform();
+    let osArchitecture: string = window.api.getArchitecture();
 
     function handleResize() {
         windowWidth = window.innerWidth;
@@ -37,12 +40,39 @@
     onDestroy(() => {
         window.removeEventListener('resize', debouncedResize);
     });
+
+    function openCrawlerHomepage():void {
+        window.api.openExternal('https://crawler.siteone.io/?utm_source=app&utm_medium='+osPlatform+'&utm_campaign='+osArchitecture+'&utm_content='+VERSION);
+    }
 </script>
 
 <div class="container max-h-screen h-screen w-full max-w-full min-w-full">
     <CrawlerForm {windowWidth} {windowHeight} bind:this={form}/>
 </div>
+<div class="footer">
+    Made by <a href="mailto:jan.reges@siteone.cz?subject=SiteOne Crawler feedback" class="text-info">Ján Regeš</a> @ 2023, <a on:click={openCrawlerHomepage} target="_blank" class="text-info cursor-pointer">crawler.siteone.io</a>
+</div>
 
 <style>
     @import "assets/global.css";
+
+    .footer {
+        position: fixed;
+        display: flex;
+        max-width: fit-content;
+        border-top-left-radius: 6px !important;
+        text-align: right;
+        justify-content: right;
+        column-gap: 4px;
+        min-width: 200px;
+        opacity: 0.4;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0, 0, 0, 0.2);
+        color: #cccccc;
+        padding: 0px 4px;
+        font-size: 12px;
+        z-index: 1100;
+    }
+
 </style>
